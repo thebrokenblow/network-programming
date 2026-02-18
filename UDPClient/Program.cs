2ee5f1535135722f52883c0ada4953aa8088ef45
Артём Красов <artkrasov@gmail.com>
2026-02-18 18:04:35 +0300

    Добавил UDP

---
+++ UDPClient/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+using var udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+var message = "Hello, World";
+byte[] data = Encoding.UTF8.GetBytes(message);
+EndPoint remotePoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5555);
+int bytes = await udpSocket.SendToAsync(data, remotePoint);
+
+Console.WriteLine($"Отправлено {bytes} байт");
+
+Console.ReadLine();
---
+++ UDPServer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+using var udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+var localIP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5555);
+udpSocket.Bind(localIP);
+Console.WriteLine("UDP-сервер запущен...");
+
+var data = new byte[256];
+EndPoint remoteIp = new IPEndPoint(IPAddress.Any, 0);
+
+var result = await udpSocket.ReceiveFromAsync(data, remoteIp);
+var message = Encoding.UTF8.GetString(data, 0, result.ReceivedBytes);
+
+Console.WriteLine($"Получено {result.ReceivedBytes} байт");
+Console.WriteLine($"Удаленный адрес: {result.RemoteEndPoint}");
+Console.WriteLine(message);
+
+Console.ReadLine();
